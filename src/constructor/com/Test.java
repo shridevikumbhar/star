685eb8306900452bf44a1916fa116d6d01e0f464
shridevikumbhar <shridevikumbhar6@gmail.com>
2021-11-19 21:37:03 +0530

    This my first code

---
+++ src/constructor/com/Test.java
@@ -0,0 +1,10 @@
+package constructor.com;
+
+public class Test {
+	Test() {
+		System.out.println("Test Constructor");
+	}
+    public static void main(String [] args)  {
+    	Test t=new Test();
+    }
+}
---
+++ src/constructor/com/Text.java
@@ -0,0 +1,12 @@
+package constructor.com;
+
+public class Text extends Test {
+    
+	Text() {
+		
+	System.out.println("Text Constructor");
+	}
+	public static void main(String [] args)  {
+		Text t=new Text();
+	}
+}
